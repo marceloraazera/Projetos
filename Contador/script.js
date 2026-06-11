f7fce7d187322766bb7445531a20cad11c4396f9
Marcelo H. Lima Razera Claudino <marceloraazer@gmail.com>
2026-06-11 08:59:53 -0300

    .

--- Contador/script.js
+++ Contador/script.js
@@ -1,10 +1,12 @@
 let numero = 0
 
-function intervalo() {
+let intervalo = setInterval(function () {
     numero += 100
+
     document.getElementById("contador").innerText = numero
-    if(numero >= 10000) {
+
+    if (numero >= 1000) {
         clearInterval(intervalo)
     }
-}
-setInterval(intervalo, 100)
+
+}, 100)
