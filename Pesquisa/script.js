83ac5cbd6a09bc02d0ca5a692f335a7705227911
Marcelo H. Lima Razera Claudino <marceloraazer@gmail.com>
2026-06-11 09:07:49 -0300

    .

--- Pesquisa/script.js
+++ Pesquisa/script.js
@@ -1,20 +1,23 @@
 let pesquisa = document.getElementById("pesquisa")
 
-pesquisa.addEventListener("keyup", function() {
-    let texto = pesquisa.value.toLowerCase()
-    let cards = document.querySelectorAll(".card")
+pesquisa.addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        let texto = pesquisa.value.toLowerCase()
+        let cards = document.querySelectorAll(".card")
 
-    let encontrados = 0
+        let encontrados = 0
 
-    cards.forEach(function(card) {
-        let titulo = card.querySelector("h2").textContent.toLowerCase()
+        cards.forEach(function (card) {
+            let titulo = card.querySelector("h2").textContent.toLowerCase()
 
-        if(titulo.includes(texto)) {
-            card.style.display = "block"
-            encontrados++
-        } else {
-            card.style.display = "none"
-        }
-    })
-    document.getElementById("resultado").innerText = encontrados + " resultados encontrados"
+            if (titulo.includes(texto)) {
+                card.style.display = "block"
+                encontrados++
+            } else {
+                card.style.display = "none"
+            }
+        })
+
+        document.getElementById("resultado").innerText = encontrados + " resultados encontrados"
+    }
 })
